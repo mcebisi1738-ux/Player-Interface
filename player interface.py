6ec507f0f4f8494b692d491a6740e5362e3670a0
Mcebisi Nsele <mcebisi1738@gmail.com>
2026-07-18 16:19:27 +0200

    Add files via upload

---
+++ player interface.py
@@ -0,0 +1,48 @@
+import random
+from abc import ABC, abstractmethod
+
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0, 0)
+        self.path = [self.position]
+
+    def make_move(self):
+        dx, dy = random.choice(self.moves)
+
+        x, y = self.position
+        self.position = (x + dx, y + dy)
+
+        self.path.append(self.position)
+
+        return self.position
+
+    @abstractmethod
+    def level_up(self):
+        pass
+
+
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+        self.moves = [
+            (0, 1),   # up
+            (0, -1),  # down
+            (1, 0),   # right
+            (-1, 0)   # left
+        ]
+
+    def level_up(self):
+        diagonal_moves = [
+            (1, 1),
+            (-1, 1),
+            (1, -1),
+            (-1, -1)
+        ]
+
+        self.moves.extend(diagonal_moves)
+
+
+        
+
+
